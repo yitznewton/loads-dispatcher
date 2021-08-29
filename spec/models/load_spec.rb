b55cb074e105b655007275cab918d62d40dac5e3
yitznewton <yitznewton@hotmail.com>
2021-08-29 17:17:36 -0400

    add Load tests

--- spec/models/load_spec.rb
+++ spec/models/load_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
-RSpec.describe Load, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Load do
+  subject { described_class.new(complete_attributes) }
+
+  let(:complete_attributes) { base_attributes.merge(attributes) }
+  let(:attributes) {{}}
+
+  describe 'validation' do
+    context 'with a lowball rate' do
+      let(:attributes) {{ rate: 9000 }}
+
+      specify do
+        expect(subject).not_to be_valid
+      end
+    end
+
+    context 'with a box truck exclusion' do
+      let(:attributes) {{ notes: 'No box trucks!!' }}
+
+      specify do
+        expect(subject).not_to be_valid
+      end
+    end
+
+    context 'with an NYC/Long Island pickup' do
+      let(:attributes) {{ pickup_location: {city: 'A', state: 'NY', county: 'Queens'} }}
+
+      specify do
+        expect(subject).not_to be_valid
+      end
+    end
+
+    context 'with an NYC/Long Island dropoff' do
+      let(:attributes) {{ dropoff_location: {city: 'A', state: 'NY', county: 'Suffolk'} }}
+
+      specify do
+        expect(subject).not_to be_valid
+      end
+    end
+  end
+
+  def base_attributes
+    {
+      pickup_date: Time.now,
+      pickup_location: {city: 'Passaic', state: 'NJ', county: 'Passaic'},
+      dropoff_location: {city: 'West Hartford', state: 'CT', county: 'Hartford'},
+      weight: 5000,
+      rate: 20000,
+      distance: 100,
+      broker_company: BrokerCompany.new(name: 'TQL'),
+      load_identifier: LoadIdentifier.new(identifier: 'ABC123')
+    }
+  end
 end
--- spec/requests/loads_spec.rb
+++ spec/requests/loads_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Loads", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/loads/index"
+RSpec.describe 'Loads', type: :request do
+  describe 'GET index' do
+    it 'returns http success' do
+      get '/'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
