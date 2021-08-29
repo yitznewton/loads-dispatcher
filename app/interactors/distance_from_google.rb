f0217fe7ed97c01f16c4ade9b21900be697aa332
yitznewton <yitznewton@hotmail.com>
2021-08-29 16:16:35 -0400

    wip

--- app/interactors/distance_from_google.rb
+++ app/interactors/distance_from_google.rb
@@ -4,17 +4,13 @@ class DistanceFromGoogle
   def self.call(origin:, destination:)
     raise "Don't call me" if Rails.env.test?
 
-    Rails.logger.debug(msg: 'Google location needed', origin: origin, destination: destination)
+    Rails.logger.debug(msg: 'Requesting location from Google', origin: origin, destination: destination)
 
-    payload = Rails.cache.fetch("distance_from_google/#{origin}/#{destination}") do
-      Rails.logger.debug(msg: 'Requesting location from Google', origin: origin, destination: destination)
-
-      url = 'https://maps.googleapis.com/maps/api/distancematrix/json'
-      params = {destinations: destination, origins: origin, units: 'imperial', key: ENV['GOOGLE_MAPS_API_KEY']}
-      response = Faraday.get(url, params)
-      JSON.parse(response.body).tap do |json|
-        raise BadGoogleDistanceResponse if json.include?('error_message')
-      end
+    url = 'https://maps.googleapis.com/maps/api/distancematrix/json'
+    params = {destinations: destination, origins: origin, units: 'imperial', key: ENV['GOOGLE_MAPS_API_KEY']}
+    response = Faraday.get(url, params)
+    payload = JSON.parse(response.body).tap do |json|
+      raise BadGoogleDistanceResponse if json.include?('error_message')
     end
 
     (payload['rows'].first['elements'].first['distance']['value'] * METERS_TO_MILES).to_i
--- app/models/places_distance.rb
+++ app/models/places_distance.rb
@@ -2,10 +2,11 @@ class PlacesDistance < ApplicationRecord
   def self.for_places(origin:, destination:)
     return new(distance: 0) if origin == destination
 
-    create(
-      origin: origin.to_s,
-      destination: destination.to_s,
+    create_with(
       distance: DistanceFromGoogle.call(origin: origin.to_s, destination: destination.to_s)
+    ).find_or_create_by(
+      origin: origin.to_s,
+      destination: destination.to_s
     )
   end
 end
