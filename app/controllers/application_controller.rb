90931a117b6251b9226fd05b04ccc52f5ece8a75
yitznewton <yitznewton@hotmail.com>
2021-09-01 17:45:36 -0400

    Simple authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  AUTH_REALM = ENV['HTTP_AUTH_REALM']
+
+  before_action :authenticate if Rails.env.production?
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_digest(AUTH_REALM) do
+      ENV['AUTH_PASSWORD_DIGEST']
+    end
+  end
 end
