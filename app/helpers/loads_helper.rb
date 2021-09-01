a64d31a4fccbc6406b5e3af62dc95835804a99d3
yitznewton <yitznewton@hotmail.com>
2021-08-31 21:32:39 -0400

    Add contact links to show

--- app/helpers/loads_helper.rb
+++ app/helpers/loads_helper.rb
@@ -7,11 +7,63 @@ module LoadsHelper
     show_cents ? "$#{whole_dollars}.#{cents}" : "$#{whole_dollars}"
   end
 
-  def load_field(load, key, label = nil)
-    value = key.is_a?(Symbol) ? load.send(key) : key
+  def load_field(load, key, label = nil, &block)
+    value = if block.present?
+              capture(&block)
+            elsif key.is_a?(Symbol)
+              load.send(key)
+            else
+              key
+            end
+
     label = key.to_s.humanize if !label
 
     tag.dt(label) + tag.dd(value)
+  end
+
+  def broker_data(load)
+    # rubocop:todo Rails/OutputSafety
+    raw([load.broker_company, load.contact_name, phone_link(load), email_link(load)].compact.join(' - '))
+    # rubocop:enable Rails/OutputSafety
+  end
+
+  def phone_link(load)
+    load.contact_phone && phone_to(load.contact_phone, number_to_phone(load.contact_phone))
+  end
+
+  def email_link(load)
+    email_url = URI('https://mail.google.com/mail')
+
+    email_url.query = {
+      view: :cm,
+      su: email_subject(load),
+      to: load.contact_email,
+      body: email_body(load)
+    }.to_query
+
+    link_to(load.contact_email || 'compose email', email_url.to_s, target: 'dispatcher_email')
+  end
+
+  def email_subject(load)
+    "Offer to deliver ref# #{load.reference_number} (#{load.pickup_location} to #{load.dropoff_location})"
+  end
+
+  def email_body(load)
+    <<~EOF
+      Posted rate: #{currency(load.rate, show_cents: false)}
+      Broker: #{load.broker_company}
+      Notes: #{load.notes}
+
+      Dear #{load.contact_name || '_____'}:
+
+      Regarding load reference number #{load.reference_number}, I have a 26' box truck in the area with a highly qualified CDL driver. Our ETA to the shipper's location (#{load.pickup_location}) is ________. I can deliver this load for you for $________.
+
+      Have a great day!
+      --
+      Peter Holmes, Dispatcher
+      Milton Morley Transport
+      973-572-7541
+    EOF
   end
 
   def load_map(load)
