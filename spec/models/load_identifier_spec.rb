7b3a7f72c873a8c868f34ed38bfc73b1ae91c949
yitznewton <yitznewton@hotmail.com>
2021-08-31 21:51:57 -0400

    Clarify LoadIdentifier soft-delete

--- spec/models/load_identifier_spec.rb
+++ spec/models/load_identifier_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe LoadIdentifier, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe LoadIdentifier do
+  describe 'destroying' do
+    let(:load_board) { LoadBoard.create(name: 'TQL') }
+
+    it 'sets deleted_at' do
+      load_identifier = described_class.create!(identifier: '123', load_board: load_board)
+      expect { load_identifier.destroy }.to(change { load_identifier.deleted_at })
+    end
+
+    it 'soft-deletes with #destroy' do
+      load_identifier = described_class.create!(identifier: '123', load_board: load_board)
+      expect { load_identifier.destroy }.not_to(change { described_class.count })
+    end
+
+    it 'soft-deletes with #destroy!' do
+      load_identifier = described_class.create!(identifier: '123', load_board: load_board)
+      expect { load_identifier.destroy! }.not_to(change { described_class.count })
+    end
+  end
 end
