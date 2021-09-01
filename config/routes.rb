323f8efddf346c88a72dfd3b8c2c77954f2e4745
Ellie Schaffer <yitznewton@users.noreply.github.com>
2021-09-01 09:46:04 -0400

    Add button to toggle display of maps (#10)
    * Add button to toggle display of maps
    
    * tweak

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   resources :loads, only: %i[index show destroy] do
     post :shortlist, on: :member
     post :unshortlist, on: :member
+
+    post :show_maps, on: :collection
+    post :hide_maps, on: :collection
   end
 end
