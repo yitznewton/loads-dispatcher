0037006f3835f64b9a609b28823197ea471e638c
Ellie Schaffer <yitznewton@users.noreply.github.com>
2021-09-09 14:44:58 -0400

    Automatically queue the next refresh job (#29)
    Closes #23

--- app/jobs/loads_data_refresh_job.rb
+++ app/jobs/loads_data_refresh_job.rb
@@ -1,5 +1,13 @@
 class LoadsDataRefreshJob < ApplicationJob
+  discard_on BadTqlResponse, BadTruckersEdgeResponse
+
   def perform(truckers_edge_auth_token:)
     CombinedRefresh.call(origin_date: Time.current, truckers_edge_auth_token: truckers_edge_auth_token)
+
+    self.class.set(wait: waiting_period).perform_later(truckers_edge_auth_token: truckers_edge_auth_token)
+  end
+
+  def waiting_period
+    rand(40..360).seconds
   end
 end
--- spec/jobs/loads_data_refresh_job_spec.rb
+++ spec/jobs/loads_data_refresh_job_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe LoadsDataRefreshJob do
+  include ActiveJob::TestHelper
+
   before do
     allow(CombinedRefresh).to receive(:call)
   end
@@ -10,6 +12,30 @@ describe LoadsDataRefreshJob do
 
     expect(CombinedRefresh).to have_received(:call) do |kwargs|
       expect(kwargs.fetch(:truckers_edge_auth_token)).to eq('ABC123')
+    end
+  end
+
+  describe 're-queuing the next refresh' do
+    context 'when there was an error' do
+      before do
+        allow(CombinedRefresh).to receive(:call).and_raise(BadTruckersEdgeResponse)
+      end
+
+      it 'does not re-queue' do
+        assert_no_enqueued_jobs do
+          described_class.perform_now(truckers_edge_auth_token: 'ABC123')
+        end
+      end
+    end
+
+    context 'when there was success' do
+      it 're-queues' do
+        # rubocop:disable RSpec/DescribedClass
+        assert_enqueued_with(job: LoadsDataRefreshJob, args: [{ truckers_edge_auth_token: 'ABC123'}]) do
+          described_class.perform_now(truckers_edge_auth_token: 'ABC123')
+        end
+        # rubocop:enable RSpec/DescribedClass
+      end
     end
   end
 end
