d8c3f1740524e1decbb191c294762b5295563d9c
Ellie Schaffer <yitznewton@users.noreply.github.com>
2021-09-10 10:40:21 -0400

    Improve error reporting for refresh errors (#38)
    * Improve error reporting for refresh errors
    
    * cop

--- app/interactors/base_refresh.rb
+++ app/interactors/base_refresh.rb
@@ -1,11 +1,12 @@
 class BaseRefresh
+  # rubocop:disable Metrics/AbcSize
   def call
     touched_ids = []
 
     Load.transaction do
       City.routes.each do |(origin_city, destination_city)|
         data = response_body(origin_city: origin_city, destination_city: destination_city)
-        raise response_exception_klass unless loads?(data)
+        raise response_exception_klass.new, data unless loads?(data)
 
         loads(data).each { |l| load_factory_klass.call(l).tap { |ll| touched_ids << ll&.id } }
       end
@@ -13,6 +14,7 @@ class BaseRefresh
       delete_untouched_loads(touched_ids)
     end
   end
+  # rubocop:enable Metrics/AbcSize
 
   def delete_untouched_loads(touched_ids)
     load_board.load_identifiers.active
