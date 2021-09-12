f8b824d940bdf2685e208caec109ba7800771b69
Ellie Schaffer <yitznewton@users.noreply.github.com>
2021-09-12 08:36:46 -0400

    Use HTTP Basic auth (#51)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,11 @@
 class ApplicationController < ActionController::Base
-  AUTH_REALM = ENV['HTTP_AUTH_REALM']
-
   before_action :authenticate if Rails.env.production?
 
   private
 
   def authenticate
-    authenticate_or_request_with_http_digest(AUTH_REALM) do
-      ENV['AUTH_PASSWORD_DIGEST']
+    authenticate_or_request_with_http_basic do
+      ENV['AUTH_PASSWORD']
     end
   end
 end
