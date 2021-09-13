bc89ec86d11c01d27f4726a3e89e04fabaf828ae
yitznewton <yitznewton@hotmail.com>
2021-09-13 14:49:20 -0400

    Re-implement refresh job

--- app/controllers/refreshes_controller.rb
+++ app/controllers/refreshes_controller.rb
@@ -5,7 +5,7 @@ class RefreshesController < ApplicationController
   def create
     return head :bad_request unless truckers_edge_auth_token
 
-    LoadsDataRefreshJob.perform_later(truckers_edge_auth_token: truckers_edge_auth_token)
+    LoadsDataRefreshJob.enqueue(truckers_edge_auth_token: truckers_edge_auth_token)
     head :accepted
   end
 
