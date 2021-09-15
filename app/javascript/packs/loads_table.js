9940e20346c9e9dd52194222da4c3606ac76e4e9
Ellie Schaffer <yitznewton@users.noreply.github.com>
2021-09-14 23:07:12 -0400

    Disable Shortlist and Dismiss buttons while AJAX is processing (#90)
    For #75

--- app/javascript/packs/loads_table.js
+++ app/javascript/packs/loads_table.js
@@ -1,11 +1,16 @@
 document.querySelectorAll('.dismiss-button-form').forEach((element) => {
-  element.addEventListener('ajax:success', function (event) {
+  /* eslint-disable no-param-reassign */
+  const buttons = Array.from(element.getElementsByClassName('dismiss-button'));
+
+  element.addEventListener('ajax:before', () => (buttons.forEach((button) => button.disabled = true)));
+  element.addEventListener('ajax:success', (event) => {
     const tr = event.target.parentElement.parentElement;
-    const { loadId } = this.dataset;
+    const { loadId } = element.dataset;
     tr.remove();
 
     if (!loadId) return;
 
     document.dispatchEvent(new Event(`load:${loadId}:delete`));
   });
+  element.addEventListener('ajax:complete', () => (buttons.forEach((button) => button.disabled = false)));
 });
--- app/javascript/packs/shortlist_buttons.js
+++ app/javascript/packs/shortlist_buttons.js
@@ -1,12 +1,22 @@
 document.querySelectorAll('.shortlist-button-form').forEach((element) => {
+  /* eslint-disable no-param-reassign */
+  const buttons = Array.from(element.getElementsByClassName('shortlist-button'));
+
+  element.addEventListener('ajax:before', () => (buttons.forEach((button) => button.disabled = true)));
   element.addEventListener('ajax:success', () => {
     element.classList.add('hidden');
     element.parentElement.querySelectorAll('.unshortlist-button-form').forEach((e) => e.classList.remove('hidden'));
   });
+  element.addEventListener('ajax:complete', () => (buttons.forEach((button) => button.disabled = false)));
 });
 document.querySelectorAll('.unshortlist-button-form').forEach((element) => {
+  /* eslint-disable no-param-reassign */
+  const buttons = Array.from(element.getElementsByClassName('unshortlist-button'));
+
+  element.addEventListener('ajax:before', () => (buttons.forEach((button) => button.disabled = true)));
   element.addEventListener('ajax:success', () => {
     element.classList.add('hidden');
     element.parentElement.querySelectorAll('.shortlist-button-form').forEach((e) => e.classList.remove('hidden'));
   });
+  element.addEventListener('ajax:complete', () => (buttons.forEach((button) => button.disabled = false)));
 });
